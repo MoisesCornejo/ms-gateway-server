3e57781154f04e4c81bbc8cc8944a07c3780b9b6
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-08 19:23:36 -0400

    Inicializar el proyecto MS-Gateway-Server con Maven y Spring Boot

---
+++ src/main/java/cl/moises/springcloud/msgatewayserver/MsGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package cl.moises.springcloud.msgatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGatewayServerApplication.class, args);
+    }
+
+}
