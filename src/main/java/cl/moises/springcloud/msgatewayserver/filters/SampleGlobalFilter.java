ec29814205177e0aa237521ff31c773dc2eb8091
MoisesCornejo <moises.cornejo@inacapmail.cl>
2025-04-08 19:27:02 -0400

    Agregar un filtro global de ejemplo para el manejo de solicitudes en Spring Cloud Gateway

---
+++ src/main/java/cl/moises/springcloud/msgatewayserver/filters/SampleGlobalFilter.java
@@ -0,0 +1,27 @@
+package cl.moises.springcloud.msgatewayserver.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class SampleGlobalFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(SampleGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Ejecutando el filtro antes del request (Pre-filter)");
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            logger.info("Ejecutando el filtro después del request (Post-filter)");
+        }));
+    }
+
+}
